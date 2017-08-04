3d9df5e1ed91f3a1f49b6ff1221f44cdf4929dbd
Kyle Grah <kylelgrah@Kyles-MacBook-Pro.local>
2017-08-03 23:19:23 -0700

    Added static image gallery

--- app/src/main/java/com/example/lukecaughell/dankbox/MainActivity.java
+++ app/src/main/java/com/example/lukecaughell/dankbox/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.lukecaughell.dankbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4"
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.doge,
+            R.drawable.nerf,
+            R.drawable.spiderman,
+            R.drawable.yodawg
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<ImageData> ImageDatas = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplication(), ImageDatas);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private ArrayList<ImageData> prepareData() {
+        ArrayList<ImageData> theImage = new ArrayList<>();
+        for (int i = 0; i < image_titles.length; i++) {
+            ImageData imageData = new ImageData();
+            imageData.setImage_title(image_titles[i]);
+            imageData.setImage_id(image_ids[i]);
+            theImage.add(imageData);
+        }
+        return theImage;
     }
 }
---
+++ app/src/main/java/com/example/lukecaughell/dankbox/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.example.lukecaughell.dankbox;
+
+import android.content.Context;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kylelgrah on 8/3/17.
+ */
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<ImageData> galleryList;
+    private Context context;
+
+    public MyAdapter(Context context, ArrayList<ImageData> galleryList) {
+        this.galleryList = galleryList;
+        this.context = context;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout,viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(galleryList.get(i).getImage_title());
+        viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        viewHolder.img.setImageResource((galleryList.get(i).getImage_id()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return galleryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+        private ImageView img;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            title = (TextView)view.findViewById(R.id.title);
+            img = (ImageView) view.findViewById(R.id.img);
+        }
+    }
+}
